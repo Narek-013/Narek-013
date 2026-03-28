69a2f541addc75440d64760cc977a94897c0c8a8
Narek Meliksetyan <narek.meliksetyan@ladmins-MacBook-Pro-4.local>
2026-03-29 01:53:56 +0400

    Cleanup profile repo: keep only profile README

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { selectedLanguageReducer } from "./slices/selectedLanguageSlice";
-
-export const store = configureStore({
-  reducer: {
-    selectedLanguage: selectedLanguageReducer,
-  },
-});
